c800e8d075e1b42d0a85560b861fab47061cd78d
Atanas-S-Nikolov <atanas.stoyanov1199@gmail.com>
2023-02-25 15:16:07 +0200

    Create breadcrumb in Products page

--- src/js/pages/Products.js
+++ src/js/pages/Products.js
@@ -2,22 +2,39 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 
-import Typography from "@mui/material/Typography";
+import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Divider from "@mui/material/Divider";
+
+import Icon from '@mui/material/Icon';
+import HomeIcon from '@mui/icons-material/Home';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 import AppFooter from "../components/footer/AppFooter";
 import NavigationBar from "../components/menu/NavigationBar";
 import ProductGrid from "../components/products/ProductGrid";
+import StyledLink from "../components/styled/StyledLink";
 
-import { convertEnumValueToKey } from "../utils/StringUtils";
 import { getNotEarlyAccessProducts, getProducts } from "../api/backend";
-import ProductCategory from "../model/product/ProductCategory";
-import ProductType from "../model/product/ProductType";
+import ProductType from "../model/product/ProductType"
+import { HOME_URL } from "../constants/UrlConstants";
+import { categoriesItems } from "../utils/categories";
+import { convertEnumValueToKey } from "../utils/StringUtils";
+import { buildProductsNavigationUrlByCategory, buildProductsNavigationUrlByCategoryAndType } from "../utils/NavigationUtils";
+
+function getIconByCategory(category) {
+  let icon;
+  categoriesItems.forEach(c => {
+    if (c.text === category) {
+      icon = c.icon;
+    }
+  })
+  return icon;
+}
 
 export default function Products() {
   const [searchParams, setSearchParams] = useSearchParams();
   const { isAuthenticated } = useSelector(state => state.authentication);
-  const [title, setTitle] = useState("");
+  const [breadcrumbTypeText, setBreadcrumbTypeText] = useState("");
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(false);
   const category = searchParams.get("category");
@@ -28,23 +45,20 @@ export default function Products() {
     loadProducts()
     .then(response => {
       const products = response.data;
-      let productCategory, productType;
+      let productType;
 
       if (products && products.length > 0) {
-        productCategory = products[0].category;
         productType = products[0].type;
       } else {
-        productCategory = convertEnumValueToKey(category);
         productType = convertEnumValueToKey(type);
       }
       
-      let pageTitle = "";
-      if (category) {
-        pageTitle += ProductCategory[productCategory].name;
-      } else if (type) {
-        pageTitle += ProductType[productType].plural;
+      if (type) {
+        setBreadcrumbTypeText(ProductType[productType].plural);
+      } else {
+        setBreadcrumbTypeText("");
       }
-      setTitle(pageTitle);
+
       setProducts(products);
       setLoading(true);
     })
@@ -60,7 +74,32 @@ export default function Products() {
             <div style={{
               margin: "1% 1% 0% 1%"
             }}>
-              <Typography variant="h5">{title}</Typography>
+              <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />}>
+                <StyledLink
+                  className="link"
+                  to={HOME_URL}
+                >
+                  <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+                  Home
+                </StyledLink>
+                <StyledLink
+                  className="link"
+                  to={buildProductsNavigationUrlByCategory(category)}
+                >
+                  <Icon sx={{ mr: 0.5 }} fontSize="inherit">{getIconByCategory(category)}</Icon>
+                  {category}
+                </StyledLink>
+                {
+                  type
+                    ? <StyledLink
+                        className="link"
+                        to={buildProductsNavigationUrlByCategoryAndType(category, type)}
+                      >
+                        {breadcrumbTypeText}
+                      </StyledLink>
+                    : null
+                }
+              </Breadcrumbs>
               <Divider sx={{ mt: 1 }}/>
               <ProductGrid items={products}/>
             </div>
---
+++ src/js/utils/NavigationUtils.js
@@ -0,0 +1,14 @@
+import { store } from "../redux/store";
+
+const state = store.getState();
+const { isAuthenticated } = state.authentication;
+
+export function buildProductsNavigationUrlByCategory(category) {
+  return isAuthenticated ? `/product?category=${category}` : `/product?early_access=false&category=${category}`;
+}
+
+export function buildProductsNavigationUrlByCategoryAndType(category, type) {
+  return isAuthenticated 
+    ? `/product?category=${category}&type=${type}`
+    : `/product?early_access=false&category=${category}&type=${type}`;
+}
