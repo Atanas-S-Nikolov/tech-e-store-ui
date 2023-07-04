b74d894630b7e7fde2dd6eeaea6545d97a094abe
Atanas Nikolov <57293300+Atanas-S-Nikolov@users.noreply.github.com>
2023-07-05 01:52:48 +0300

    Fix product image picker (#36)

--- src/js/components/products/ProductImagePicker.jsx
+++ src/js/components/products/ProductImagePicker.jsx
@@ -1,19 +1,104 @@
-import "@/styles/products/ProductImagePicker.css";
+import { useState } from 'react';
 
-import IconButton from "@mui/material/IconButton";
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
 
-export default function ProductImagePicker({ images }) {
+export default function SwipeableImagePicker({ images }) {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+  const maxSteps = images.length;
+
   return (
-    <div className="image-picker">
-      <IconButton><KeyboardArrowLeftIcon /></IconButton>
-      {images?.map(image => {
-        const imageUrl = image.url;
-        return <a href={imageUrl} key={crypto.randomUUID()}><img className="image" src={imageUrl} alt={imageUrl} /></a>
-      })}
-      <IconButton><KeyboardArrowRightIcon /></IconButton>
-    </div>
+    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 80,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      >
+      </Paper>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        interval={4000}
+        enableMouseEvents
+      >
+        {images.map((image, index) => {
+          const imageUrl = image.url;
+          return (
+          <div key={crypto.randomUUID()}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                className='product-image'
+                component="img"
+                sx={{
+                  aspectRatio: '1 / 1',
+                  borderRadius: '5px',
+                  height: 400,
+                  display: 'block',
+                  maxWidth: 400,
+                  overflow: 'hidden',
+                  // width: '100%',
+                }}
+                src={imageUrl}
+                alt={imageUrl}
+              />
+            ) : null}
+          </div>
+          )
+        })}
+      </SwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <IconButton
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </IconButton>
+        }
+        backButton={
+          <IconButton onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+          </IconButton>
+        }
+      />
+    </Box>
   );
+
+  function handleNext() {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  function handleBack() {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  function handleStepChange(step) {
+    setActiveStep(step);
+  };
 }
